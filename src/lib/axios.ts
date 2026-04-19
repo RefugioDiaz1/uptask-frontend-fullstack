c6d71ba1458da6b16432054278a14a8ffdbbac8a
Refugio Diaz <refugiodelaodiaz@gmail.com>
2026-04-18 18:09:31 -0600

    p

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,7 +5,7 @@ const api = axios.create({
 })
 
 api.interceptors.request.use(config => {
-
+    console.log(import.meta.env.VITE_API_URL)
     const token = localStorage.getItem('AUTH_TOKEN')
     if(token){
         config.headers.Authorization = `Bearer ${token}`
